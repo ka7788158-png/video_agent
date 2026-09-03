fb890e8a778a65de317a04e0afe909f9a4839feb
ka7788158-png <ka7788158@gmail.com>
2026-09-03 23:32:45 +0530

    adding comments for better understanding

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -1,13 +1,13 @@
 # accept any audio/video and 
 import yt_dlp
-from pydub import AudioSegment
+from pydub import AudioSegment # going to use for chunking later 
 import os
 
-DOWNLOAD_DIR = "downloades"
+DOWNLOAD_DIR = "downloades" # directory for saving all the downloads from this
 os.makedirs(DOWNLOAD_DIR, exist_ok = True)
 
 def download_youtube_audio(url : str) -> str:
-    output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
+    output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s") # whatever be the name of the actual file
     ydl_opts = {
         "format" : "bestaudio/best",
         "outtmpl" : output_path,
@@ -18,7 +18,7 @@ def download_youtube_audio(url : str) -> str:
                 "prefferedquality" : "192",
             }
         ],
-        "quiet" : True # hides how everything (above code{prolog}) is working
+        "quiet" : True # hides how everything (above code{prolog}) is working, remove this if you dont want to see the progress
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download = True)
@@ -32,9 +32,9 @@ def download_youtube_audio(url : str) -> str:
 def convert_to_wav(input_path : str) -> str:
     """convert any audio/video file to WAV format using pydub."""
     output_path = os.path.splitext(input_path)[0]
-    audio = AudioSegment.from_file(input_path)
-    audio = audio.set_channels(1).set_frame_rate(16000)
-    audio.export(output_path, format = "wav")
+    audio = AudioSegment.from_file(input_path) # detect the type of file you are giving 
+    audio = audio.set_channels(1).set_frame_rate(16000) # set to mono audio , set to 16 khz 
+    audio.export(output_path, format = "wav") 
     return output_path
 
 def chunk_audio(wav_path : str, chunk_minutes: int = 10) -> list:
@@ -52,6 +52,8 @@ def chunk_audio(wav_path : str, chunk_minutes: int = 10) -> list:
 
     return chunks
 
+# we want to create a function which can convert dual audio to mono audio and preferable to our whisper ai model 
+# whisper ai model -> prefers 16 khz
 def process_input(source: str) -> list:
     if source.startswith("http://") or source.startswith("https://"):
         print("Detected YouTube URL, Downloading audio....")
